5dc664e84a4b24a1ebfad9af6d6595498d7405e1
kmetop <119588911@qq.com>
2014-04-21 16:45:38 +0800

    project configuration.

---
+++ web/www_xinhouziguan_com/config/ioc.js
@@ -0,0 +1,11 @@
+{
+	bizEngine : {// 业务引擎：SFT实现
+		type : "com.kmetop.demsy.comlib.impl.engine.SFTBizEngine",
+		fields: {
+			pkgs: ["com.kmetop.demsy.comlib.impl.entity"
+					, "com.kmetop.demsy.comlib.impl.base"
+					, "com.kmetop.demsy.comlib.impl.sft"
+				]
+		}
+	}
+}
---
+++ web/www_yunnanbaiyao_com_cn/config/ioc.js
@@ -0,0 +1,13 @@
+{
+	bizEngine : {// 业务引擎：SFT实现
+		type : "com.kmetop.demsy.comlib.impl.engine.SFTBizEngine",
+		fields: {
+			pkgs: ["com.kmetop.demsy.comlib.impl.entity"
+					, "com.kmetop.demsy.comlib.impl.base"
+					, "com.kmetop.demsy.comlib.impl.sft"
+					, "com.jiongsoft.cocit.entity.sms"
+					, "com.jiongsoft.ynby.entity"
+				]
+		}
+	}
+}
